2e5a8026836ef379d61857e1659fdd68d7e3591f
Hadeer-Elsaeed <hadeerelsaeed00@gmail.com>
2020-05-08 14:50:02 +0200

    connect mongodb server

--- app.js
+++ app.js
@@ -13,7 +13,19 @@ const port = process.env.PORT || 8080;
 app.listen(port,()=>{
     console.log(`app starting listen to post  ${port}....`)
 });
+//connect to mongoose db
+mongoose.connect('mongodb://localhost:27017/GuestBook',{useNewUrlParser: true,
+useUnifiedTopology: true  })
 
+    .then((data)=>{
+        console.log("connection sucess");
+    })
+    .catch((err)=>{
+        console.log(err +"this is error");
+
+    })
+
+/* --------------------------------------- */
 
 
 //Middleware
